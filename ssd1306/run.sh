cec37f520085008171a9e04e79eaaafd4fae3d18
iotah <writeforever@foxmail.com>
2023-05-24 14:17:14 +0800

    ssd1306 fb driver is okay
    Signed-off-by: iotah <writeforever@foxmail.com>

--- ssd1306/run.sh
+++ ssd1306/run.sh
@@ -3,6 +3,11 @@
 echo "building..."
 make
 
+if [ "$?" != "0" ]; then
+    echo "build failed! exiting..."
+    exit -1
+fi
+
 echo "reinstalling mod ..."
 sudo rmmod ssd1306_fb
 sudo insmod ssd1306_fb.ko
